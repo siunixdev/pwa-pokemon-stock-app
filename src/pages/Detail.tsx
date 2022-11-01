6689e015b327866396aca5df61f8317a7067c234
siunixdev <abd.siunix@gmail.com>
2022-11-01 18:53:33 +0800

    read param on detail page

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { ArrowLeftIcon } from '@heroicons/react/24/outline'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 export const Detail = () => {
+  const param = useParams()
+  const name = param.pokeId
+
   return (
     <React.Fragment>
       <div className='relative flex items-center justify-center p-4 shadow'>
@@ -14,7 +17,7 @@ export const Detail = () => {
         <h3 className='text-xl font-bold text-gray-800'>Stok Pokémon</h3>
       </div>
       <div className='container mx-auto p-5'>
-        <h1 className='text-4xl font-bold text-gray-800 my-6'>Pikachu</h1>
+        <h1 className='text-4xl font-bold text-gray-800 my-6 capitalize'>{name?.split('-').join(' ')}</h1>
         <div className='bg-gray-100 w-fit px-4 py-2 border border-gray-300 rounded shadow cursor-pointer'>
           <span className='text-cyan-800 text-sm font-medium'>Update stok</span>
         </div>
