773fc9af56f4c89dd10dfef7e9b92f6b2afce614
siunixdev <abd.siunix@gmail.com>
2022-11-01 18:48:49 +0800

    inital data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,28 +3,45 @@ import { Link } from 'react-router-dom'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 
 interface Pokemon {
-  name: string
+  name: string,
+  stok: number
 }
 
 const Home = () => {
   const pokeUrl = 'https://pokeapi.co/api/v2/pokemon'
-  const [next, setNext] = useState<string>('')
-  const [prev, setPrev] = useState<string>('')
   const [pokemon, setPokemon] = useState<Pokemon[]>([])
+  const [filteredPokemon, setFilteredPokemon] = useState<Pokemon[]>([])
+  const [keyword, setKeyword] = useState<string>('')
 
   const fetchPokemon = (url: string) => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        setPokemon(data.results)
-        setNext(data.next)
-        setPrev(data.previous)
+        const pokemonData: { name: string; stok: number }[] = []
+
+        data.results.forEach((dt: { name: string }) => {
+          pokemonData.push({
+            name: dt.name,
+            stok: 0,
+          })
+        });
+
+        setPokemon(pokemonData)
+        setFilteredPokemon(pokemonData)
       })
   }
 
   useEffect(() => {
-    fetchPokemon(pokeUrl)
-  }, [])
+    if(keyword.trim() === '') {
+      fetchPokemon(pokeUrl)
+    }
+  }, [keyword])
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if(event.key === 'Enter') {
+      setFilteredPokemon(pokemon.filter(poke => poke.name.includes(keyword)))
+    }
+  }
 
   return (
     <React.Fragment>
@@ -33,7 +50,7 @@ const Home = () => {
           <h1 className='text-3xl md:text-4xl font-bold text-gray-800'>Stok Pokémon</h1>
         </div>
         <div className='mt-10'>
-          <input type="text" name="" id="" className='border border-gray-500 outline-none px-4 py-2 rounded text-gray-700 w-full md:w-min' placeholder='Search Pokémon' />
+          <input type="text" name="" id="" value={keyword} onChange={e => setKeyword(e.target.value)} onKeyDown={handleKeyDown} className='border border-gray-500 outline-none px-4 py-2 rounded text-gray-700 w-full md:w-min' placeholder='Search Pokémon' />
           <table width="100%" className='mt-8'>
             <thead>
               <tr className='border-b border-gray-500 flex justify-between'>
@@ -43,39 +60,17 @@ const Home = () => {
             </thead>
             <tbody>
               {
-                pokemon.map((poke, i) => (
+                filteredPokemon.map((poke, i) => (
                   <Link to={`${poke.name}`} key={i}>
                     <tr className='border-b border-gray-400 flex justify-between'>
                       <td className='my-4 text-cyan-800 font-semibold capitalize'>{poke.name.split('-').join(' ')}</td>
-                      <td className='my-4'>{i}</td>
+                      <td className='my-4'>{poke.stok}</td>
                     </tr>
                   </Link>
                 ))
               }
             </tbody>
           </table>
-          <div className='flex justify-between mt-8'>
-            <div className={`flex items-center justify-center gap-2 bg-gray-100 text-cyan-800 w-fit px-4 py-2 border border-gray-300 rounded shadow ${!prev ? 'text-gray-400 cursor-not-allowed' : 'cursor-pointer'}`}
-              onClick={() => {
-                if (prev) {
-                  fetchPokemon(prev)
-                }
-              }}
-            >
-              <ChevronLeftIcon className='w-4 h-4' />
-              <span className='text-sm font-medium'>Prev</span>
-            </div>
-            <div className={`flex items-center justify-center gap-2 bg-gray-100 text-cyan-800 w-fit px-4 py-2 border border-gray-300 rounded shadow ${!next ? 'text-gray-400 cursor-not-allowed' : 'cursor-pointer'}`}
-              onClick={() => {
-                if (next) {
-                  fetchPokemon(next)
-                }
-              }}
-            >
-              <span className='text-sm font-medium'>Next</span>
-              <ChevronRightIcon className='w-4 h-4' />
-            </div>
-          </div>
         </div>
       </div>
     </React.Fragment>
