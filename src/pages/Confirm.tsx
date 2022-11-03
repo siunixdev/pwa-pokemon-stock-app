cc7bbdc83e03b90d26af3d26decfa0651983a0d2
siunixdev <abd.siunix@gmail.com>
2022-11-03 09:55:17 +0800

    Update confirm page

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import { XMarkIcon, ArrowRightIcon, PencilIcon } from '@heroicons/react/24/outline'
 import Button from '../components/Button'
 import PokemonContext from '../context/PokemonContext'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import dayjs from 'dayjs'
 
 const Confirm = () => {
@@ -82,77 +82,94 @@ const Confirm = () => {
 
   return (
     <React.Fragment>
-      <div className='relative flex items-center justify-center p-4 shadow md:hidden'>
-        <div className='absolute left-5 text-cyan-800'>
-          <XMarkIcon className='w-6 h-6' onClick={handleClose} />
-        </div>
-        <h3 className='text-xl font-bold text-gray-800'>Stok Pokémon</h3>
-      </div>
-      <div className='container mx-auto w-auto md:w-1/2 p-8'>
-        <h1 className='title'>Konfirmasi Update stok</h1>
-        <div className='mt-8'>
-          <span>Selisih</span>
-          <h1 className='text-2xl md:text-4xl text-gray-800 capitalize'>+{pokemonUpdatedStok} pcs</h1>
-        </div>
-        <div className='mt-10'>
-          <div className='grid grid-cols-2'>
-            <div>
-              <p className='text-xs md:text-base'>Di Sistem</p>
-              <h3 className='text-xl text-gray-800'>{pokemonUpdateStok} pcs</h3>
-            </div>
-            <div className='flex gap-8'>
-              <div className='flex justify-center items-center'>
-                <ArrowRightIcon className='w-6 h-6' />
+      {
+        (pokemonLusin !== 0 || pokemonPcs !== 0) ? (
+          <>
+            <div className='relative flex items-center justify-center p-4 shadow md:hidden'>
+              <div className='absolute left-5 text-cyan-800'>
+                <XMarkIcon className='w-6 h-6' onClick={handleClose} />
               </div>
-              <div>
-                <p className='text-xs md:text-base'>Hasil Update Stok</p>
-                <h3 className='text-xl text-gray-800'>{pokemonUpdateStok + pokemonUpdatedStok} pcs</h3>
+              <h3 className='text-xl font-bold text-gray-800'>Stok Pokémon</h3>
+            </div>
+            <div className='container mx-auto w-auto md:w-1/2 p-8'>
+              <h1 className='title'>Konfirmasi Update stok</h1>
+              <div className='mt-8'>
+                <span>Selisih</span>
+                <h1 className='text-2xl md:text-4xl text-gray-800 capitalize'>+{pokemonUpdatedStok} pcs</h1>
+              </div>
+              <div className='mt-10'>
+                <div className='grid grid-cols-2'>
+                  <div>
+                    <p className='text-xs md:text-base'>Di Sistem</p>
+                    <h3 className='text-xl text-gray-800'>{pokemonUpdateStok} pcs</h3>
+                  </div>
+                  <div className='flex gap-8'>
+                    <div className='flex justify-center items-center'>
+                      <ArrowRightIcon className='w-6 h-6' />
+                    </div>
+                    <div>
+                      <p className='text-xs md:text-base'>Hasil Update Stok</p>
+                      <h3 className='text-xl text-gray-800'>{pokemonUpdateStok + pokemonUpdatedStok} pcs</h3>
+                    </div>
+                  </div>
+                </div>
+                <div className='mt-12'>
+                  <h3 className='text-sm md:text-xl font-bold text-gray-800'>Detail stok opname</h3>
+                  <table width="100%" className='mt-8 text-gray-700'>
+                    <thead>
+                      <tr className='border-b border-gray-500 flex justify-between'>
+                        <th className='my-4 text-gray-700 w-1/2 text-left'>Keterangan</th>
+                        <th className='my-4'>Jumlah</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        <tr className='border-b border-gray-400 flex justify-between'>
+                          <td className='my-4 w-1/2'>
+                            <p className='text-cyan-800 font-semibold capitalize'>Hasil update stok</p>
+                            <p className='text-sm'>{pokemonPcs} pcs, {pokemonLusin} lusin (12pcs)</p>
+                          </td>
+                          <td className='flex items-center gap-3 my-4 text-cyan-800 text-center'>
+                            <span>+{pokemonUpdatedStok} pcs</span>
+                            <div className='cursor-pointer' onClick={handleEdit}>
+                              <PencilIcon className='w-4 h-4' />
+                            </div>
+                          </td>
+                        </tr>
+                      }
+                      <tr className='flex justify-between'>
+                        <td className='my-4 w-1/2 font-semibold'>Total hasil stok opname</td>
+                        <td className='my-4 text-cyan-800 text-center'>{pokemonUpdateStok + pokemonUpdatedStok} pcs</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div className='mt-10'>
+                  <h3 className='font-bold text-gray-800 mb-4'>Catatan</h3>
+                  <textarea name="" id="" rows={3} value={description} onChange={e => setDescription(e.target.value)} className='w-full border border-gray-200 outline-none p-4 rounded-md'></textarea>
+                </div>
+                <div className='flex justify-end items-center mt-4 gap-2'>
+                  <div className='invisible md:visible'>
+                    <Button text='Batal' type='secondary' handle={handleClose} />
+                  </div>
+                  <Button text='Simpan' type='primary' handle={handleSave} />
+                </div>
               </div>
             </div>
-          </div>
-          <div className='mt-12'>
-            <h3 className='text-sm md:text-xl font-bold text-gray-800'>Detail stok opname</h3>
-            <table width="100%" className='mt-8 text-gray-700'>
-              <thead>
-                <tr className='border-b border-gray-500 flex justify-between'>
-                  <th className='my-4 text-gray-700 w-1/2 text-left'>Keterangan</th>
-                  <th className='my-4'>Jumlah</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  <tr className='border-b border-gray-400 flex justify-between'>
-                    <td className='my-4 w-1/2'>
-                      <p className='text-cyan-800 font-semibold capitalize'>Hasil update stok</p>
-                      <p className='text-sm'>{pokemonPcs} pcs, {pokemonLusin} lusin (12pcs)</p>
-                    </td>
-                    <td className='flex items-center gap-3 my-4 text-cyan-800 text-center'>
-                      <span>+{pokemonUpdatedStok} pcs</span>
-                      <div className='cursor-pointer' onClick={handleEdit}>
-                        <PencilIcon className='w-4 h-4' />
-                      </div>
-                    </td>
-                  </tr>
-                }
-                <tr className='flex justify-between'>
-                  <td className='my-4 w-1/2 font-semibold'>Total hasil stok opname</td>
-                  <td className='my-4 text-cyan-800 text-center'>{pokemonUpdateStok + pokemonUpdatedStok} pcs</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div className='mt-10'>
-            <h3 className='font-bold text-gray-800 mb-4'>Catatan</h3>
-            <textarea name="" id="" rows={3} value={description} onChange={e => setDescription(e.target.value)} className='w-full border border-gray-200 outline-none p-4 rounded-md'></textarea>
-          </div>
-          <div className='flex justify-end items-center mt-4 gap-2'>
-            <div className='invisible md:visible'>
-              <Button text='Batal' type='secondary' handle={handleClose} />
+          </>
+        ) : (
+          <>
+            <div className='pokemon-container'>
+              <p className='text-gray-600 text-center mb-4'>Halaman konfirmasi tidak dapat ditampilkan!</p>
+              <Link to={'/'}>
+                <p className='text-cyan-800 text-center underline'>
+                  Kembali ke daftar Pokemon
+                </p>
+              </Link>
             </div>
-            <Button text='Simpan' type='primary' handle={handleSave} />
-          </div>
-        </div>
-      </div>
+          </>
+        )
+      }
     </React.Fragment>
   )
 }
